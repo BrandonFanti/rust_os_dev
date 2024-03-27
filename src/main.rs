5cd70ba2f8c5061995b3749a9410a03e2eea56c7
brandon <brandon@brandonfanti.net>
2024-03-27 06:30:11 -0400

    Added  - isa-debug qemu exit  - serial/uart out (panics and headless ops)  - vga out  - custom tests    - "temporarily" removed panic=abort from Cargo.toml, until [this](https://github.com/rust-lang/cargo/issues/7359) gets fixed

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,83 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello Operator!";
+use core::{panic::PanicInfo, ptr};
+mod vga_buffer;
+mod serial;
+
+fn main() -> u8 {
+    println!("Hello Operator!");
+    serial_println!("Hello computer!\n");
+
+    #[cfg(test)]
+    test_main();
+
+    panic!("AAHHH");
+    return 0;
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 
 //new main
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+
+    main();
 
     loop {}
 }
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    print!("{}", info);
     loop {}
 }
+
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
